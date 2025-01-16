002c8158cb5728b1bc8c5feeff1de9f1af6967d6
Alexander Wong <alexwongsydney@gmail.com>
2025-01-16 20:56:04 +0900

    Added some more logic for list deletion

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "lists#index"
 
-  resources :lists, only: [:index, :new, :show, :create] do
+  resources :lists, only: [:index, :new, :show, :create, :destroy] do
     resources :bookmarks, only: [:create]
   end
 
